a87575d324b40d943feba2a8be86a149af52af84
elissandrello <elissandrello@andreani.com>
2022-04-05 23:33:54 -0300

    Corrección del contador

---
+++ src/componentes/ItemCount.js
@@ -0,0 +1,51 @@
+import { Button } from '@material-ui/core';
+import { Add, Remove } from '@material-ui/icons';
+import { useEffect, useState } from 'react';
+import styled from "styled-components";
+
+
+const ContainerProducts = styled.div`
+    display: flex;
+    align-items: center;
+    margin: 20px;
+`;
+
+const ProductQuantity = styled.div`
+    font-size: 24px;
+    margin: 5px;
+`;
+
+const ItemCount = ({ stock = 5, initial = 1, onAdd }) => {
+    const [count, setCount] = useState([]);
+
+    useEffect(() => {
+        setCount(initial);
+    }, [initial]);
+
+    function increment() {
+        if (count < stock) {
+            setCount(count + 1);
+        }
+    }
+
+    const decrement = () => {
+        if (count > initial) {
+            setCount(count - 1);
+        }
+    }
+    return (
+        <ContainerProducts>
+            <Button variant="text" onClick={decrement}><Remove /></Button>
+            <ProductQuantity>{count}</ProductQuantity>
+            <Button variant="text" onClick={increment}><Add /></Button>
+            {
+                stock
+                ? <Button variant="contained" color="secondary" onClick={() => onAdd(count)}>Agregar al Carrito</Button>
+                : <Button variant="contained" disabled>Agregar al Carrito</Button>
+            }
+
+        </ContainerProducts >
+    );
+}
+
+export default ItemCount;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import '../App.css';
 import NavBar from '../componentes/NavBar.js';
 import ItemListContainer from '../componentes/ItemListContainer.js';
+import ItemCount from '../componentes/ItemCount';
 
 function Home() {
   return (
@@ -8,6 +9,7 @@ function Home() {
       <div>
         <NavBar />
         <ItemListContainer contenido="Texto de prueba" />
+        <ItemCount />
       </div>
     </>
   );
