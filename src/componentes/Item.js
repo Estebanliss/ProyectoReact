1d6e79fe52f5b2cc9ad2fbc733b4517a0ffa5e5f
elissandrello <elissandrello@andreani.com>
2022-04-09 18:55:26 -0300

    Se sumaron productos e Imagenes

--- src/componentes/Item.js
+++ src/componentes/Item.js
@@ -1,13 +1,47 @@
+import { DescriptionOutlined, InfoOutlined, LocalOfferOutlined } from '@material-ui/icons';
+import styled from "styled-components";
 
 const Items = ({ title, stock, price, pictureUrl }) => {
 
+    const ContainerItem = styled.div`
+        background: red;
+    `;
+
+    const ImageItem = styled.img`
+    height: 300px;
+
+    `;
+
+    const IconItem = styled.div`
+        font-size: 12px;
+        width: 100px;
+        height: 40px;
+        background-color: white;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        margin: 10px;
+        transition: all 0.5s ease;
+        &:hover {
+            background-color: #e9f5f5;
+            transform: scale(1.3);
+        }
+    `;
+
     return (
-        <div>
-            <img src={pictureUrl} alt="Imagen"></img>
-            <p><strong>${title}</strong></p>
-            <p><strong>${price}</strong></p>
-            <p><strong>${stock} unid.</strong></p>
-        </div>
+        <ContainerItem>
+            <ImageItem src={pictureUrl} />
+            <IconItem>
+                <LocalOfferOutlined /><strong>${price}</strong>
+            </IconItem>
+            <IconItem>
+                <DescriptionOutlined /><strong>${title}</strong>
+            </IconItem>
+            <IconItem>
+                <InfoOutlined /><strong>${stock} unid.</strong>
+            </IconItem>
+
+        </ContainerItem>
 
     )
 
--- src/utils/products.js
+++ src/utils/products.js
@@ -1,269 +1,84 @@
 const products = [
     {
-      id: 30,
-      name: "Agua Con Gas 1,5 lts",
-      stock: 0,
-      cost: 140,
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.",
-      capacity: 1500,
+      id: 1,
+      name: "Spectacular Spider-Man Facsimile Edition (2022) #1",
+      stock: 16,
+      cost: 3.99,
+      description: "By the mid-1970s, Spidey fans could thrill to their favorite hero’s escapades in AMAZING SPIDER-MAN, his high-octane encounters with other super-types in MARVEL TEAM-UP, his all-ages adventures in SPIDEY SUPER STORIES and reprints of classic stories in MARVEL TALES – and still it wasn’t enough! Such was the clamor that a second solo title debuted with a focus on the man beneath the mask: the always popular Peter Parker! But fear not, there’s plenty of costumed action in this frantic first issue – and when the deadly Tarantula strikes at Peter’s university, it’s Spidey who feels the sting! Will the Tarantula claim the mayor’s life, or can our wall-crawling Web-Head save the day in suitably spectacular fashion? It’s one of the all-time great Marvel comic books, boldly re-presented in its original form, ads and all! Reprinting SPECTACULAR SPIDER-MAN (1976) #1.",
       image: [
-        "https://res.cloudinary.com/bodegas-del-sur/image/upload/v1631316114/BodegasDelSur/01_1619706181.webp"
+        "https://i.annihil.us/u/prod/marvel/i/mg/2/90/62432e610ac07/clean.jpg"
       ],
-      sales: 0,
-      createdAt: "2021-09-18T05:11:04.952Z",
-      updatedAt: "2021-09-18T19:39:05.347Z",
-      categoryId: 9,
-      brandId: 95,
-      packingId: null,
-      category: {
-        name: "Sin Alcohol",
-        id: 9
-      },
-      brand: {
-        id: 95,
-        name: "Eco de los Andes"
-      },
-      reviews: []
+      sales: 10.000,
+      categoryId: "Comic",
+      Rating: "Rated T",
     },
     {
-      id: 29,
-      name: "Agua Sin Gas 1,5 lts",
-      stock: 100,
-      cost: 140,
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.",
-      capacity: 1500,
+      id: 2,
+      name: "Deadpool: Bad Blood (2022) #1",
+      stock: 23,
+      cost: 3.99,
+      description: "Deadpool’s smash-hit, first-ever original graphic novel – now serialized in comics form! Wade Wilson has been shooting, stabbing and otherwise annoying people for a long time. He’s made a lot of enemies. But one he just can’t quite place is the brutal Thumper, who keeps showing up out of the blue to pound him into jelly. What is Deadpool’s past connection to this beefy face masher? Wade has as much of a clue as you do! So he decides to call in some help from an old friend: Domino! Superstar co-creator of the Merc with a Mouth, Rob Liefeld, tells his greatest Deadpool tale yet!",
       image: [
-        "https://res.cloudinary.com/bodegas-del-sur/image/upload/v1631316114/BodegasDelSur/01_1619706076.webp"
+        "https://i.annihil.us/u/prod/marvel/i/mg/f/03/62432e8e49266/clean.jpg"
       ],
-      sales: 0,
-      createdAt: "2021-09-18T05:11:04.952Z",
-      updatedAt: "2021-09-18T19:39:05.347Z",
-      categoryId: 9,
-      brandId: 95,
-      packingId: null,
-      category: {
-        name: "Sin Alcohol",
-        id: 9
-      },
-      brand: {
-        id: 95,
-        name: "Eco de los Andes"
-      },
-      reviews: [
-        {
-          "comment": "Muy rico y refrescante",
-          "rating": "4"
-        }
-      ]
+      sales: 6.300,
+      categoryId: "Comic",
+      Rating: "Parental Advisory",
     },
     {
-      id: 76,
-      name: "Alambrado Chardonnay 750 ml",
-      stock: 92,
-      cost: 575,
-      description: "Se muestra con tonos amarillo verdoso de intensidad media. En la nariz encontramos frutas blancas maduras, peras, manzanas y banana, frutas tropicales, con notas de la maduración en barricas sobre las borras como vainilla, manteca y miel. Buen volumen, complejo, maduro, con una entrada untuosa en la boca, buen desarrollo y largo final. Se repiten las notas frutales del aroma.",
-      capacity: 750,
+      id: 3,
+      name: "Amazing Spider-Man Facsimile Edition (2022) #238",
+      stock: 122,
+      cost: 3.99,
+      description: "GWEN STACY: SPIDER-WOMAN! In one universe, it wasn't Peter Parker bitten by the radioactive Spider, but Gwen Stacy! She's smart, charming and can lift a car-- Just don't tell her Police Chief father! How is she involved in Spider-Verse? Seeds of AMAZING SPIDER-MAN #9 are planted here!",
       image: [
-        "https://res.cloudinary.com/bodegas-del-sur/image/upload/v1631316095/BodegasDelSur/01_1605541215.jpg"
+        "https://i.annihil.us/u/prod/marvel/i/mg/9/00/62432ea15bdc9/clean.jpg"
       ],
-      sales: 0,
-      createdAt: "2021-09-18T05:11:04.952Z",
-      updatedAt: "2021-09-20T16:10:21.030Z",
-      categoryId: 1,
-      brandId: 10,
-      packingId: null,
-      category: {
-        name: "Vinos",
-        id: 1
-      },
-      brand: {
-        id: 10,
-        name: "Alambrado"
-      },
-      reviews: []
+      sales: 3.122,
+      categoryId: "Comic",
+      Rating: "Rated T",
     },
     {
-      id: 170,
-      name: "Alambrado Malbec 750ml",
-      stock: 100,
-      cost: 575,
-      description: "Rojo violáceo, profundo y vivaz con visos azulados. Nariz de frutos rojos y negros como cerezas, cassis, ciruelas y moras, integrados con los aromas de maduración en barrica como vainilla, tabaco y chocolate. Entrada en boca sedosa, buena estructura, con una marcada presencia de frutas rojas, taninos amables y dulces, concluyendo en un largo final de boca.",
-      capacity: 750,
+      id: 4,
+      name: "Captain Marvel Annual (2022) #1",
+      stock: 232,
+      cost: 3.99,
+      description: "Captain Marvel…behind bars?! Carol has gotten herself into an intergalactic scale of trouble with the K’rasky military and now she’s locked up in their city-like prison…with the Starjammers. Will they be able to blast their way out? Or will they be lost to the authoritarian labyrinth of K’rasky law?",
       image: [
-        "https://res.cloudinary.com/bodegas-del-sur/image/upload/v1631316095/BodegasDelSur/01_1605540337.jpg"
+        "https://i.annihil.us/u/prod/marvel/i/mg/9/10/62432e9acd0a8/clean.jpg"
       ],
-      sales: 0,
-      createdAt: "2021-09-18T05:11:04.952Z",
-      updatedAt: "2021-09-20T16:10:21.032Z",
-      categoryId: 1,
-      brandId: 10,
-      packingId: null,
-      category: {
-        name: "Vinos",
-        id: 1
-      },
-      brand: {
-        id: 10,
-        name: "Alambrado"
-      },
-      reviews: []
+      sales: 1.122,
+      categoryId: "Comic",
+      Rating: "Rated T+",
     },
     {
-      id: 233,
-      name: "Alambrado Malbec 750 ml con Estuche",
-      stock: 100,
-      cost: 765,
-      description: "La linea Alambrado esta compuesta por vinos elegantes y de estilo moderno, donde se destacan claramente las características frutales de las variedades que los componen. Su paso por madera les otorgan una complejidad muy particular, acompañando de manera armónica y sutil la calidad obtenida desde el viñedo. Poseen un potencial de guarda de entre 4 y 6 años.\nAlambrado Malbec fue elaborado a partir de una cuidosa selección de uvas cosechadas de forma manual y criado en barricas de roble francés durante 10 meses.",
-      capacity: 750,
+      id: 5,
+      name: "The Thing (2021) #6",
+      stock: 232,
+      cost: 3.99,
+      description: "MARVEL UNIVERSE GOES OUT WITH A BANG! It all comes down to this! The secret power behind Mot is revealed at last – but with it comes a very personal threat to Ben Grimm! Can the Thing battle his way past the one foe that waits for all of us and snatch victory from the jaws of defeat? And in order to do so, will it mean working hand-in-glove with Doctor Doom?",
       image: [
-        "https://res.cloudinary.com/bodegas-del-sur/image/upload/v1631316116/BodegasDelSur/01_1621366255.jpg"
+        "https://i.annihil.us/u/prod/marvel/i/mg/3/f0/62432e4f2879d/clean.jpg"
       ],
-      sales: 0,
-      createdAt: "2021-09-18T05:11:04.952Z",
-      updatedAt: "2021-09-20T16:10:21.031Z",
-      categoryId: 1,
-      brandId: 10,
-      packingId: null,
-      category: {
-        name: "Vinos",
-        id: 1
-      },
-      brand: {
-        id: 10,
-        name: "Alambrado"
-      },
-      reviews: []
+      sales: 1.122,
+      categoryId: "Comic",
+      Rating: "Rated T+",
     },
     {
-      id: 139,
-      name: "Altos del Plata Cabernet Sauvignon 750 ml",
-      stock: 100,
-      cost: 490,
-      description: "Color: Rojo rubí con sutiles reflejos terracota. Aroma: Se destacan los aromas a frutas negras como higos, ciruelas y confitura de moras, armónicamente acompañados por notas de pimiento rojo, regaliz y tostado. Boca: Se percibe la untuosidad de la fruta madura junto a una elegante estructura tánica y persistencia en boca.",
-      capacity: 750,
+      id: 6,
+      name: "Marvel's Midnight Suns | Gameplay Reveal",
+      stock: 232,
+      cost: 10.99,
+      description: "Deep dive into the gameplay experience of ‘Marvel’s Midnight Suns’ - an epic tactical RPG set in the darker side of the Marvel Universe.",
       image: [
-        "https://res.cloudinary.com/bodegas-del-sur/image/upload/v1631316118/BodegasDelSur/01_1623090927.jpg"
+        "https://terrigen-cdn-dev.marvel.com/content/prod/1x/midnightsons_lob_crd_02.jpg"
       ],
-      sales: 0,
-      createdAt: "2021-09-18T05:11:04.952Z",
-      updatedAt: "2021-09-20T16:10:21.030Z",
-      categoryId: 1,
-      brandId: 16,
-      packingId: null,
-      category: {
-        name: "Vinos",
-        id: 1
-      },
-      brand: {
-        id: 16,
-        name: "Altos del Plata"
-      },
-      reviews: []
+      sales: 1.122,
+      categoryId: "Game",
+      Rating: "Rated T+",
     },
-    {
-      id: 68,
-      name: "Altos del Plata Chardonnay 750 ml",
-      stock: 89,
-      cost: 490,
-      description: "Altos del Plata Chardonnay pertenece a la bodega Terrazas de los Andes. Su vino es de color amarillo dorado con destellos verdes. Su perfil fresco y frutado revela notas a flores blancas como jazmín y aromas a pera, durazno blanco y ananá. Acompañan sutiles notas tostadas y dulces como vainilla y miel. Presenta ligeros toques tostados y una acidez persistente.",
-      capacity: 750,
-      image: [
-        "https://res.cloudinary.com/bodegas-del-sur/image/upload/v1631316117/BodegasDelSur/01_1623084032.jpg"
-      ],
-      sales: 0,
-      createdAt: "2021-09-18T05:11:04.952Z",
-      updatedAt: "2021-09-20T16:10:21.032Z",
-      categoryId: 1,
-      brandId: 16,
-      packingId: null,
-      category: {
-        name: "Vinos",
-        id: 1
-      },
-      brand: {
-        id: 16,
-        name: "Altos del Plata"
-      },
-      reviews: []
-    },
-    {
-      id: 222,
-      name: "Altos del Plata Malbec 375 ml",
-      stock: 100,
-      cost: 300,
-      description: "Altos del Plata Malbec pertenece a la bodega Terrazas de los Andes. Su vino es de color rojo con destellos morados. Predomina la presencia de aromas a fruta negra como ciruela y mora en armonía con un toque tostado. Se distinguen notas especiadas a pimienta blanca.",
-      capacity: 375,
-      image: [
-        "https://res.cloudinary.com/bodegas-del-sur/image/upload/v1631316127/BodegasDelSur/01_1626713403.jpg"
-      ],
-      sales: 0,
-      createdAt: "2021-09-18T05:11:04.952Z",
-      updatedAt: "2021-09-20T16:10:21.032Z",
-      categoryId: 1,
-      brandId: 16,
-      packingId: null,
-      category: {
-        name: "Vinos",
-        id: 1
-      },
-      brand: {
-        id: 16,
-        name: "Altos del Plata"
-      },
-      reviews: []
-    },
-    {
-      id: 135,
-      name: "Altos del Plata Malbec 750 ml",
-      stock: 100,
-      cost: 490,
-      description: "Altos del Plata Malbec pertenece a la bodega Terrazas de los Andes. Su vino es de color rojo con destellos morados. Predomina la presencia de aromas a fruta negra como ciruela y mora en armonía con un toque tostado. Se distinguen notas especiadas a pimienta blanca.",
-      capacity: 750,
-      image: [
-        "https://res.cloudinary.com/bodegas-del-sur/image/upload/v1631316098/BodegasDelSur/01_1605808301.png"
-      ],
-      sales: 0,
-      createdAt: "2021-09-18T05:11:04.952Z",
-      updatedAt: "2021-09-20T16:10:21.032Z",
-      categoryId: 1,
-      brandId: 16,
-      packingId: null,
-      category: {
-        name: "Vinos",
-        id: 1
-      },
-      brand: {
-        id: 16,
-        name: "Altos del Plata"
-      },
-      reviews: []
-    },
-    {
-      id: 31,
-      name: "Amareto Disaronno 700 ml",
-      stock: 100,
-      cost: 3150,
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.",
-      capacity: 700,
-      image: [
-        "https://res.cloudinary.com/bodegas-del-sur/image/upload/v1631316082/BodegasDelSur/01_1600449560.jpg"
-      ],
-      sales: 0,
-      createdAt: "2021-09-18T05:11:04.952Z",
-      updatedAt: "2021-09-18T11:08:17.792Z",
-      categoryId: 5,
-      brandId: 17,
-      packingId: null,
-      category: {
-        name: "Licores",
-        id: 5
-      },
-      brand: {
-        id: 17,
-        name: "Amareto Disaronno"
-      },
-      reviews: []
-    }
+
+    
   ];
 
   module.exports = {
